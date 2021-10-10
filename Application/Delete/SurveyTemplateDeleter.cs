a51296d22e430168b0dd294336238573002443c4
Wilver Martinez <wilver.martinezg@gmail.com>
2021-10-09 22:24:23 -0600

    Agregue caso de uso para eliminar una plantilla de encuesta

--- Application/Delete/SurveyTemplateDeleter.cs
+++ Application/Delete/SurveyTemplateDeleter.cs
@@ -1,11 +1,25 @@
+using System.Threading.Tasks;
+using SurveyWS.Domain.Entities.SurveyTemplate;
+using SurveyWS.Domain.Repository;
+
 namespace SurveyWS.Application.Delete
 {
     public class SurveyTemplateDeleter
     {
-        
-        public SurveyTemplateDeleter()
+        private readonly ISurveyTemplateRepository _surveyTemplateRepository;
+
+        public SurveyTemplateDeleter(ISurveyTemplateRepository surveyTemplateRepository)
         {
-            
+            _surveyTemplateRepository = surveyTemplateRepository;
+        }
+
+        public async Task Delete(long id)
+        {
+            // 1. Validamos el ID de la entidad a actualizar
+            var entityId = SurveyTemplateId.ValueOf(id);
+            entityId.Validate();
+
+            await _surveyTemplateRepository.Delete(entityId);
         }
     }
 }
